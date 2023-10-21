c000cbf6795eba52bfeb4569fdb5308cad6c4040
Thilo Danner <thilo.danner@dlr.de>
2023-10-21 23:47:12 +0200

    Reading, Conversion and Printing works

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::str;
 
 use gtk4 as gtk;
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Builder, Button, FileChooserAction, FileChooserDialog, ResponseType, TextView, FileFilter};
+use gtk::{gio, glib, Application, ApplicationWindow, Builder, Button, FileDialog, ResponseType, TextView, FileFilter};
 
 use svg2gcode::{svg2program, ConversionOptions, ConversionConfig, Machine};
 use roxmltree::Document;
@@ -34,51 +34,59 @@ pub fn build_ui(application: &Application) {
     let text_view: TextView = builder.object("text_view").expect("Couldn't get text_view");
     let send_button: Button = builder.object("send_button").expect("Couldn't get builder");
 
-    // send_button.connect_clicked(glib::clone!(@weak window, @weak text_view => move |_|{
-    //     // TODO
-    //     return;
-    // }));
+    send_button.connect_clicked(glib::clone!(@weak window, @weak text_view => move |_|{
+        // TODO
+        return;
+    }));
 
     open_button.connect_clicked(glib::clone!(@weak window, @weak text_view => move |_| {
         // Create a new file chooser dialog
-        let file_chooser = FileChooserDialog::new(
-            Some("Choose SVG-File"),
-            Some(&window),
-            FileChooserAction::Open,
-            &[("Open", ResponseType::Ok), ("Cancel", ResponseType::Cancel)],
-        );
-
-        // Add a filter to only show SVG files
-        let filter = FileFilter::new();
+        let filter = gtk::FileFilter::new();
         filter.add_mime_type("image/svg+xml"); //See: https://stackoverflow.com/questions/11918977/right-mime-type-for-svg-images-with-fonts-embedded
-        filter.set_name(Some("SVG-Dateien"));
-        file_chooser.add_filter(&filter);
+        filter.set_name(Some("SVG File"));
+        
+        let filters = gio::ListStore::new::<gtk::FileFilter>();
+        filters.append(&filter);
 
-        let mut string_vector: Vec<String> = Vec::new(); 
+        let file_chooser = gtk::FileDialog::builder()
+            .title("Choose SVG-File")
+            .accept_label("Open")
+            .filters(&filters)
+            .build();
     
         // Connect the response signal to handle the user's choice
-        file_chooser.connect_response(|file_chooser, ResponseType::Ok| {
-            file_chooser_converter(file_chooser, &text_view, &mut string_vector)
-        });       
-        window.show();
+        file_chooser.open(Some(&window), gio::Cancellable::NONE, move |file| {
+            if let Ok(file) = file {
+                let filename = file.path().expect("Couldn't get file path");
+                let file = File::open(filename).expect("Couldn't open file");
 
+                let mut reader = BufReader::new(file);
+                let mut contents = String::new();
+                let _ = reader.read_to_string(&mut contents);
+
+                let g_code_vec = file_converter(contents);
+
+                let mut g_code_string = String::new();
+
+                for x in g_code_vec.iter() {
+                    g_code_string.push_str(x.as_str());
+                    g_code_string.push_str("\n");
+                }
+
+                text_view.buffer().set_text(&g_code_string);
+            }
+        });        
     }));
+
+
+    window.present();
+
 }
 
 
-pub fn file_chooser_converter(d: &FileChooserDialog, text_view: &TextView, string_vector: &mut Vec<String>) {
-    let mut string = String::new();
+pub fn file_converter(contents: String) -> Vec<String> {
     let mut document = Document::parse("");
-
-    let file = d.file().expect("Couldn't get file");
-    let filename = file.path().expect("Couldn't get file path");
-    let file = File::open(&filename.as_path()).expect("Couldn't open file");
-
-    let mut reader = BufReader::new(file);
-    let mut contents = String::new();
-    let _ = reader.read_to_string(&mut contents);
-
-    println!("File contents:\n{}", contents);
+    let mut string_vector: Vec<String> = Vec::new();
     
     document = Document::parse(&contents.as_str());
     
@@ -91,18 +99,9 @@ pub fn file_chooser_converter(d: &FileChooserDialog, text_view: &TextView, strin
         svg2program(&document.unwrap(), &config, options, machine);
 
     for x in program.iter() {
-        // Append the current element to the existing text in the text_view
-        string.push_str(x.to_string().as_str());
-        // Append a newline to separate each element on a new line
-        string.push_str("\n");
         string_vector.push(x.to_string());
     }
 
-    text_view.buffer().set_text(&string);
-
-    d.show();
-    d.close();
-
-    // return (d.clone(), ResponseType::Ok);
+    return string_vector;
 }
 
